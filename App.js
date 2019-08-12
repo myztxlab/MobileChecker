bff163e37e9368314c1734dd5c5fcffaca168bb8
myztxlab <myztxlab@gmail.com>
2019-08-12 20:18:48 +0700

    fresh from the oven

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Container, Button, Text } from 'native-base';
+export default class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Button>
+          <Text>
+            Button
+          </Text>
+        </Button>
+      </Container>
+    );
+  }
+}
